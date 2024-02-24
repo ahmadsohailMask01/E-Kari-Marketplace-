6b7b5fe99dea0402f7f31ca6b511c362d05fbf63
ahmadsohailMask01 <ahmaddd.sohail759@gmail.com>
2024-02-25 00:05:28 +0500

    new

--- src/app/components/Admin_products.js
+++ src/app/components/Admin_products.js
@@ -1,16 +1,30 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import AdminProductslist from "./AdminProductslist";
 import styles from "../../../styles/products.module.css";
+import loading from "../../../assets/loading.gif";
 import Link from "next/link";
+import Image from "next/image";
 
 const Admin_Products = () => {
+  useEffect(() => {
+    const loading_func = () => {
+      document.getElementById("loading").style.display = "none";
+      document.getElementById("container").style.display = "flex";
+    };
+    setTimeout(loading_func, 2000);
+  }, []);
   return (
     <>
       <section className={styles.section_products}>
         <Link href={"/addProduct"} className={styles.btn} title="Add Product">
           Add Product
         </Link>
-        <div className={styles.products_container}>
+        <div className={styles.load} id="loading">
+          <span>Loading</span>
+          <Image src={loading} className={styles.loading_gif}></Image>
+        </div>
+        <div className={styles.products_container} id="container">
           <AdminProductslist />
         </div>
       </section>
