e4009b16f87139960f6ccf7be5c2990b5d1d08e2
ahmadsohailMask01 <ahmaddd.sohail759@gmail.com>
2024-03-09 12:31:19 +0500

    header changes done

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -4,35 +4,55 @@ import styles from "../../../styles/header.module.css";
 import logo from "../../../assets/images/Logo/Logo.png";
 import Image from "next/image";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { IoSearchSharp } from "react-icons/io5";
 
 export default function Header() {
   const { status } = useSession();
   return (
     <nav className={styles.nav}>
       <div className={styles.header}>
-        <div className={styles.row}>
-          <Image className={styles.logo} src={logo} width={50} height={50} />
-          <h1 className={styles.LogoText}>E-Kari</h1>
+        <Link href={"/"} className={styles.ekarilink}>
+          <div className={styles.row}>
+            <Image className={styles.logo} src={logo} width={50} height={50} />
+            <h1 className={styles.LogoText}>E-Kari</h1>
+          </div>
+        </Link>
+        <div className={styles.search}>
+          <input
+            placeholder="Find the Art you love, by Artist"
+            type="text"
+            className={styles.search_inp}
+          />
+          <button className={styles.search_btn}>
+            <IoSearchSharp style={{ color: `#f3f3f3`, fontSize: `20px` }} />
+          </button>
         </div>
         <div className={styles.NavBarLinks}>
           <Link className={styles.link_style} href="/">
-            Home
+            Explore Art
           </Link>
           <Link className={styles.link_style} href="/products">
-            Buy Item
+            Shop
           </Link>
           <Link className={styles.link_style} href="/addProduct">
-            Create Item
+            Create
           </Link>
           <Link className={styles.link_style} href="/about">
-            About
+            About Us
           </Link>
           <Link className={styles.link_style} href="/user">
             User
           </Link>
+          <Link className={styles.link_style} href="/about">
+            Contact
+          </Link>
           {status === "authenticated" ? (
             <>
-              <button onClick={() => signOut()} className={styles.link_style}>
+              <button
+                onClick={() => signOut()}
+                className={styles.logout_btn}
+                title="Logout"
+              >
                 Logout
               </button>
             </>
@@ -41,37 +61,45 @@ export default function Header() {
               <Link className={styles.link_style} href="/user_login">
                 Login
               </Link>
-              <Link className={styles.link_style} href="/signup">
-                Register
-              </Link>
             </>
           )}
         </div>
         <div className={styles.col_500}>
           <div className={styles.NavBarLinks_500}>
             <Link className={styles.link_style} href="/">
-              Home
+              Explore Art
             </Link>
             <Link className={styles.link_style} href="/products">
-              Buy Item
+              Shop
             </Link>
             <Link className={styles.link_style} href="/addProduct">
-              Create Item
+              Create
             </Link>
             <Link className={styles.link_style} href="/about">
-              About
+              About Us
             </Link>
           </div>
           <div className={styles.NavBarLinks_500}>
             <Link className={styles.link_style} href="/user_dashboard">
               User
             </Link>
-            <Link className={styles.link_style} href="/user_login">
-              Login
-            </Link>
-            <Link className={styles.link_style} href="/signup">
-              Register
-            </Link>
+            {status === "authenticated" ? (
+              <>
+                <button
+                  onClick={() => signOut()}
+                  className={styles.logout_btn}
+                  title="Logout"
+                >
+                  Logout
+                </button>
+              </>
+            ) : (
+              <>
+                <Link className={styles.link_style} href="/user_login">
+                  Login
+                </Link>
+              </>
+            )}
           </div>
         </div>
       </div>
