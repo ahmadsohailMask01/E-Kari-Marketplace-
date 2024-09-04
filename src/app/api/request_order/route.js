67b4e37f68646e9bfef6def2e80632dd7c23c2ac
ahmadsohailMask01 <ahmaddd.sohail759@gmail.com>
2024-09-04 23:15:58 +0500

    Nodemailer changes done!

--- src/app/api/request_order/route.js
+++ src/app/api/request_order/route.js
@@ -21,7 +21,7 @@ export async function POST(req, res) {
   try {
     await transporter.sendMail({
       ...mailOptions,
-      sender: "Client",
+      from: "Client",
       subject: "New Order",
       text: "New Order is Confirmed!",
       html: `<!DOCTYPE html>
