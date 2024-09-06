6877e89b0687fa8112d6f1e4f1df8f75728c1f7c
ahmadsohailMask01 <ahmaddd.sohail759@gmail.com>
2024-09-06 06:02:32 +0500

    New ProductList Change

--- src/app/components/Products_List.js
+++ src/app/components/Products_List.js
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "../../../styles/products.module.css";
 import Link from "next/link";
 import { BASE_API_URL } from "../../../utils/constants";
+import Image from "next/image";
 
 const getProducts = async () => {
   try {
@@ -22,7 +23,7 @@ const getProducts = async () => {
 
 const Productslist = async () => {
   const { products } = await getProducts();
-  console.log(products);
+  //console.log(products);
   const curr = Intl.NumberFormat("en-PK", {
     style: "currency",
     currency: "PKR",
@@ -38,19 +39,34 @@ const Productslist = async () => {
               key={t._id}
               title={`Order ID = ${t._id}`}
             >
-              <span className={`${styles.product_title} ${styles.select}`}>
-                {t.product_title}
-              </span>
-              <span className={styles.select}>
-                <strong className={styles.select}>Price:</strong>{" "}
-                {curr.format(t.product_price)} PKR
-              </span>
-              <span
-                className={`${styles.product_description} ${styles.select}`}
-              >
-                <strong className={styles.select}>Description:</strong>{" "}
-                {t.product_description}
-              </span>
+              <div className={styles.owner}>
+                <span style={{ color: `darkgray` }}>
+                  Product By: <b style={{ color: `red` }}>{t.userName}</b>
+                </span>
+              </div>
+              <div className={styles.prodInformationRow}>
+                <Image
+                  src={t.image}
+                  width={250}
+                  height={200}
+                  className={styles.prodImage}
+                />
+                <div className={styles.prodInfoCol}>
+                  <span className={`${styles.product_title} ${styles.select}`}>
+                    {t.product_title}
+                  </span>
+                  <span className={styles.select}>
+                    <strong className={styles.select}>Price:</strong>{" "}
+                    {curr.format(t.product_price)} PKR
+                  </span>
+                  <span
+                    className={`${styles.product_description} ${styles.select}`}
+                  >
+                    <strong className={styles.select}>Description:</strong>{" "}
+                    {t.product_description}
+                  </span>
+                </div>
+              </div>
               <Link
                 href={`/request_order/${t._id}`}
                 className={styles.request_button}
